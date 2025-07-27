73a6b6e75bd37bc0af47a142bafd8c3e5c350a08
BobMakhlin <borismakhlin5@gmail.com>
2025-07-27 16:43:17 +0200

    init api project

---
+++ ocean-scope-api/src/main/java/com/bobmakhlin/oceanscopeapi/OceanScopeApiApplication.java
@@ -0,0 +1,13 @@
+package com.bobmakhlin.oceanscopeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OceanScopeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OceanScopeApiApplication.class, args);
+	}
+
+}
